721a44ec39e8a4c9dcbb3909e43205762170ba9c
Dln <prod408@yandex.ru>
2024-12-06 21:39:12 +0400

    Add react-app, add gui with electron

---
+++ gui/index.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  // Запуск локального React-сервера
+  mainWindow.loadURL("http://localhost:4000");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
